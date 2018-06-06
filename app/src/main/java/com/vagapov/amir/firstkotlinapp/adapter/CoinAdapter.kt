bba9f06355893462064aea3a73d35e1869de5408
Amirka <Vagapovamir@yandex.ru>
2018-06-07 00:22:23 +0400

    refactoring in 1.0.1

--- app/src/main/java/com/vagapov/amir/firstkotlinapp/adapter/CoinAdapter.kt
+++ app/src/main/java/com/vagapov/amir/firstkotlinapp/adapter/CoinAdapter.kt
@@ -7,11 +7,10 @@ import com.vagapov.amir.firstkotlinapp.R
 import com.vagapov.amir.firstkotlinapp.model.CoinCap
 import kotlinx.android.synthetic.main.item_coin.view.*
 
-class CoinAdapter : RecyclerView.Adapter<MyViewHolder>()
-{
+class CoinAdapter : RecyclerView.Adapter<MyViewHolder>() {
 
-    var data : List<CoinCap>?=null
-        set(value){
+    var data: List<CoinCap>? = null
+        set(value) {
             field = value
             notifyDataSetChanged()
             ///comment for branch 1.0
@@ -22,19 +21,18 @@ class CoinAdapter : RecyclerView.Adapter<MyViewHolder>()
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        data?.get(position)?.let {
-            coin ->
-            holder.view.tvName.text=coin.name
-            holder.view.tvSymbol.text=coin.symbol
-            holder.view.tvPrice.text="$${coin.price_usd}"
-            holder.view.tvPriceChange.text= if(coin.percent_change_24h > 0)
+        data?.get(position)?.let { coin ->
+            holder.view.tvName.text = coin.name
+            holder.view.tvSymbol.text = coin.symbol
+            holder.view.tvPrice.text = "$${coin.price_usd}"
+            holder.view.tvPriceChange.text = if (coin.percent_change_24h > 0)
                 "+${coin.percent_change_24h}"
             else
                 "${coin.percent_change_24h}%"
         }
     }
 
-    override fun getItemCount(): Int = data?.size?: 0
+    override fun getItemCount(): Int = data?.size ?: 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_coin, parent, false))
